3d7763c26da06ec7f07f1dda16790bf39599bbfd
megzodrow <73080395+megzodrow@users.noreply.github.com>
2021-04-01 13:55:20 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+
+function addNumbers() {
+    var two = 2;
+    var three = 3;
+
+    var five = two + three;
+
+    console.log(five);    
+}
+
+addNumbers();
+
+
+function stringConcat(){
+  var taco = 'taco' ;
+  var cat = 'cat';
+  
+  var tacoCat = taco + " " + cat ;
+  
+  alert(tacoCat);
+}
+
+stringConcat();
+
+
